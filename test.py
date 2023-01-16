945c73ed9d534fda2f48383f52b3629dbec7c688
Chase Lamkin <chaselamkin@Chases-MacBook-Pro.local>
2023-01-15 20:35:44 -0600

    progress update before new test.py changes

--- test.py
+++ test.py
@@ -47,15 +47,34 @@ class Tokenizer:
                 #self.time_frame_start becomes the year day for the first day of the upcoming month
                 #self.time_frame_end becomes the year day for the last day of the upcoming month
                 if self.current_date.month == 12:
-                    next_month = self.current_date.replace(year=self.current_date.year+1, month=1)
+                    self.time_frame_start = 1
+                    self.time_frame_end = 31
                 else:
-                    next_month = self.current_date.replace(month=self.current_date.month+1)
-                self.time_frame_start = (next_month - datetime(next_month.year, 1, 1) + timedelta(days=1)).days
-                if next_month.month == 12:
-                    last_day_of_next_month = datetime(year=next_month.year+1, month=1, day=1) - timedelta(days=1)
-                else:
-                    last_day_of_next_month = datetime(year=next_month.year, month=next_month.month+1, day=1) - timedelta(days=1)
-                self.time_frame_end = (last_day_of_next_month - datetime(next_month.year, 1, 1) + timedelta(days=1)).days
+                    next_month = self.current_date.replace(month=self.current_date.month+1, day=1)
+                    self.time_frame_start = next_month.timetuple().tm_yday
+                    self.time_frame_end = self.time_frame_start + calendar.monthrange(next_month.year, next_month.month)[1] - 1
+        else:
+            if any(month in self.tokens for month in calendar.month_name[1:]) and any(x.isdigit() for x in self.tokens):
+                #if month and day specified are before current date assign self.token_error to 2
+                #if month and day specified are invalid assign self.token_error to 3
+                #self.time_frame_start becomes exact date specified
+                #self.time_frame_end becomes exact date specified
+            elif any(month in self.tokens for month in calendar.month_name[1:]):
+                #if month specified is before current date assign self.token_error to 2
+                #self.time_frame_start becomes first anual day of month specified or the current anual day if the month is the current month
+                #self.time_frame_end becomes last anual day of month specified
+            elif "week" in self.tokens:
+                #self.time_frame_start becomes the year day for the closest monday that is not the current year day
+                #self.time_frame_start becomes the year day for the sunday in the same week as self.time_frame_start
+            elif "month" in self.tokens:
+                #self.time_frame_start becomes the year day for the first day of the upcoming month
+                #self.time_frame_end becomes the year day for the last day of the upcoming month
+            else:
+                #self.time_frame_start becomes current year day
+                #self.time_frame_end becomes last year day of year
+
+                
+                
 
 
         print(self.time_frame_start)
@@ -63,6 +82,6 @@ class Tokenizer:
 
 
 
-input_string = "What lab is next month"
+input_string = "What lab is next week"
 tokenizer = Tokenizer(input_string)
 tokenizer.tokenize()
